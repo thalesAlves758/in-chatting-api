f6f7fd6b909995b45a371820437d4e4df50266f3
Thales Alves <thalesalves758@gmail.com>
2022-10-03 08:35:27 -0300

    feat: use routes and error handler middleware

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,14 @@
 import express, { Application } from 'express';
+import 'express-async-errors';
 import cors from 'cors';
+import routes from './routes/index.routes';
+import errorHandler from './middlewares/errorHandler.middleware';
 
 const app: Application = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(routes);
+app.use(errorHandler);
 
 export default app;
