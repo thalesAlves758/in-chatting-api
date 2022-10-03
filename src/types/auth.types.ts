245ba0d1d0c66e162803916dc3102e190bad921b
Thales Alves <thalesalves758@gmail.com>
2022-10-03 09:02:26 -0300

    feat: create sign in types

--- src/types/auth.types.ts
+++ src/types/auth.types.ts
@@ -3,3 +3,9 @@ import { User } from '@prisma/client';
 export type SignUpInsertData = Omit<User, 'id'>;
 
 export type SignUpBody = SignUpInsertData & { confirmPassword: string };
+
+export type SignInBody = Pick<User, 'email' | 'password'>;
+
+export type SignInResponse = Omit<User, 'password'> & { token: string };
+
+export type AuthJwtPayload = Pick<User, 'id'>;
