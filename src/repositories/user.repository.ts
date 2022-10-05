8dd9e5f6a58f0d7e9e7ba276b50167f1dadde64c
Thales Alves <thalesalves758@gmail.com>
2022-10-05 18:25:22 -0300

    feat: create a function to find a user by id

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -6,6 +6,10 @@ export async function findByEmail(email: string): Promise<User | null> {
   return prisma.user.findUnique({ where: { email } });
 }
 
+export async function findById(id: number): Promise<User | null> {
+  return prisma.user.findUnique({ where: { id } });
+}
+
 export async function insert(data: SignUpInsertData): Promise<void> {
   await prisma.user.create({ data });
 }
