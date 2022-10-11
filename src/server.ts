b3216eee655cc15e03f6c583ffab6a4bc370f08a
Thales Alves <thalesalves758@gmail.com>
2022-10-11 20:15:31 -0300

    feat: init Io

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,16 @@
 import app from './app';
 import dotenv from 'dotenv';
+import http from 'http';
+import { initIo } from './io';
 
 dotenv.config();
 
+const server = http.createServer(app);
+
 const port = process.env.PORT ?? 5000; /* eslint-disable-line */
 
-app.listen(port, () => {
+initIo(server);
+
+server.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
