68eafc0a415efab864a0484fca6676e44c0da80c
Thales Alves <thalesalves758@gmail.com>
2022-10-13 20:03:12 -0300

    feat: create io types

---
+++ src/types/io.types.ts
@@ -0,0 +1,9 @@
+/* eslint-disable */
+export enum IoEvents {
+  CONNECTION = 'connection',
+  CONNECTION_ERROR = 'connection_error',
+  PRIVATE_MESSAGE = 'private_message',
+  RECEIVED_PRIVATE_MESSAGE = 'received_private_message',
+  MESSAGE_SUCCESS = 'message_success',
+  MESSAGE_ERROR = 'message_error',
+}
