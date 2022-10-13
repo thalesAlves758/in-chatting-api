22e42162d83226b73bc26b39e51e3bdbdeeec92d
Thales Alves <thalesalves758@gmail.com>
2022-10-13 20:14:22 -0300

    feat: add cors config

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,11 @@ import errorHandler from './middlewares/errorHandler.middleware';
 const app: Application = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN_URL,
+  })
+);
 app.use(routes);
 app.use(errorHandler);
 
