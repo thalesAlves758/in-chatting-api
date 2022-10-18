4dca833aedae1890796913ed929b278337c7ebb8
Thales Alves <thalesalves758@gmail.com>
2022-10-18 10:35:33 -0300

    feat: remove sawAt property from MessageSent and MessageReceived types

--- src/types/message.types.ts
+++ src/types/message.types.ts
@@ -1,7 +1,7 @@
 import { Message } from '@prisma/client';
 
-export type MessageSent = Omit<Message, 'id' | 'fromUserId'>;
+export type MessageSent = Omit<Message, 'id' | 'fromUserId' | 'sawAt'>;
 
 export type MessageInsertData = Omit<Message, 'id'>;
 
-export type MessageReceived = Omit<Message, 'id' | 'toUserId'>;
+export type MessageReceived = Omit<Message, 'id' | 'toUserId' | 'sawAt'>;
