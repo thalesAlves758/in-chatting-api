4a1758aed646daaffc8a6ba71450c91b8a46bfbd
Thales Alves <thalesalves758@gmail.com>
2022-10-18 10:25:04 -0300

    feat: create UserWithMessages type

--- src/types/user.types.ts
+++ src/types/user.types.ts
@@ -1,3 +1,7 @@
-import { User } from '@prisma/client';
+import { User, Message } from '@prisma/client';
 
 export type UserInfo = Omit<User, 'password'>;
+
+export type UserWithMessages = UserInfo & {
+  lastMessages: Message[];
+};
