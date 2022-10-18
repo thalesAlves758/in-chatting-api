05cb3c09cea266f9bf034e2f676b788991a8ad10
Thales Alves <thalesalves758@gmail.com>
2022-10-18 10:28:29 -0300

    feat: create getChatsByUserId function

--- src/services/message.services.ts
+++ src/services/message.services.ts
@@ -1,7 +1,15 @@
 import { insert } from '../repositories/message.repository';
+import { findWithWhomThereWasChat } from '../repositories/user.repository';
 import { MessageInsertData } from '../types/message.types';
+import { UserWithMessages } from '../types/user.types';
 import { encryptText } from '../utils/cryptr.utils';
 
 export async function insertMessage(data: MessageInsertData): Promise<void> {
   await insert({ ...data, text: encryptText(data.text) });
+}
+
+export async function getChatsByUserId(
+  userId: number
+): Promise<UserWithMessages[]> {
+  return findWithWhomThereWasChat(userId);
 }
