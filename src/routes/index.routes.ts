4bcfcefb42b63165fb2e641c7c42ff9a5ef18087
Thales Alves <thalesalves758@gmail.com>
2022-10-18 10:31:52 -0300

    feat: import and use messages router

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import authRouter from './auth.routes';
+import messagesRouter from './messages.routes';
 
 const router: Router = Router();
 
 router.use(authRouter);
+router.use(messagesRouter);
 
 export default router;
